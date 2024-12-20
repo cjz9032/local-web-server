ba749f60ef052288ade898dbcee654effa224690
Xiuting XT9 Chen <chenxt9@lenovo.com>
2024-12-20 13:25:14 +0800

    feat: clear csp

---
+++ lws-plugins/clear-csp.js
@@ -0,0 +1,41 @@
+import EventEmitter from 'events'
+
+class CSP extends EventEmitter {
+  description() {
+    return 'Support for modifying Content Security Policy (CSP) headers to remove restrictions.'
+  }
+
+  optionDefinitions() {
+    return []
+  }
+
+  middleware(config) {
+    const cspOptions = {
+      'default-src': config.cspDefaultSrc || '*',
+      'script-src': config.cspScriptSrc || "'unsafe-inline' *",
+      'object-src': config.cspObjectSrc || '*',
+      'base-uri': config.cspBaseUri || '*',
+      'style-src': config.cspStyleSrc || "'unsafe-inline' *",
+      'img-src': config.cspImgSrc || "* data:",
+      'connect-src': config.cspConnectSrc || '*',
+      'frame-src': config.cspFrameSrc || '*'
+    }
+    this.emit('verbose', 'middleware.csp.config', cspOptions)
+  
+    return async function (ctx, next) {
+      const originalWriteHead = ctx.res.writeHead
+
+      ctx.res.writeHead = function (statusCode, headers) {
+        headers = headers || {};
+        headers['content-security-policy'] = Object.entries(cspOptions)
+          .map(([key, value]) => `${key} ${value}`)
+          .join('; ')
+        return originalWriteHead.call(this, statusCode, headers)
+      }
+
+      await next()
+    }
+  }
+}
+
+export default CSP
